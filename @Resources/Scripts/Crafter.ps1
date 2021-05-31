1c9de071ca73c88a5a6e6ff42becdd443bed5b3a
deathcrafter <sahoodeathcrafter@users.noreply.github.com>
2021-05-31 08:05:30 +0530

    vector successful

--- @Resources/Scripts/Crafter.ps1
+++ @Resources/Scripts/Crafter.ps1
@@ -4,25 +4,35 @@ function Update {
 function main {
 
 }
-funtion craft {
+
+function Vectorize {
+    Craft -type vector
+}
+
+function Craft {
     param(
         [Parameter(Mandatory=$true)]
         [ValidateSet('bar', 'round', 'vector')]
-        $type='bar',
+        $type,
         [Parameter()]
         [ValidateSet('normal','reflection','mirrorX','mirrorY','mirrorXY')]
-        $barType,
-        [Parameter()]
-        $combine
+        $barType
     )
+    $resources=$RmAPI.VariableStr('@')
+
     $measureContent = @"
 "@
+
     $bands = $RmAPI.Variable("Bands")
+
     switch ($type) {
         'vector' {
             $layerCount=$RmAPI.Variable("LayerCount")
+
+            $k=$RmAPI.Variable("VectorBands")
+
             for ($j=1; $j -lt $layerCount+1; $j++) {
-                for ($i=1; $i -lt ($bands/$j)+1; $i++) {
+                for ($i=1; $i -lt $k+1; $i++) {
                     $measureContent+=@"
 
 [MeasureL$j$i]
@@ -32,11 +42,13 @@ Parent=AudioAnalyzer
 Type=Child
 Index=$i
 Channel=Auto
-HandlerName=MainLayer$($j)Output
+HandlerName=Layer$($j)Output
 
 "@
                 }
+                $k=2*$k-1
             }
+            $measureContent | Out-File -FilePath $($resources + 'Measures\AudioMeasures.inc') -Encoding utf8
             break
         }
         default {
@@ -56,8 +68,7 @@ HandlerName=MainFinalOutput
             }
         }
     }
-    $shapeContent=@"
-"@
+
     switch ($type) {
         'bar' {
             
@@ -84,6 +95,10 @@ HandlerName=MainFinalOutput
             $cornerRadius = $RmAPI.Variable("CornerRounding")
         
             $minimumHeight = $RmAPI.Variable("MinimumHeight")
+
+            $shapeContent=@"
+"@
+
             switch ($barType) {
                 'normal' {
                     $a = $barWidth + $barGap
@@ -101,21 +116,15 @@ TransformationMatrix=$(Math -f cos -v $(Math -f rad -v $(-$angle%360)));$(Math -
 
 "@
                     for ($i=1; $i -lt $bands+1; $i++) {
-                        if ($i -eq 1) {
                         $shapeContent+=@"
 
-Shape=Rectangle $($barStrokeWidth/2), $((1-$flipY)*$barHeight), $barWidth, ($(-1+2*$flipY)*Clamp($barHeight*[Measure$(if ($flipX -eq 0){$i}else{$bands-$i+1})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color1]
+Shape$(if ($i -ne 1){$i}else{''})=Rectangle $(if($i -eq 1){$barStrokeWidth/2}else{($i-1)*$a}), $((1-$flipY)*$barHeight), $barWidth, ($(-1+2*$flipY)*Clamp($barHeight*[Measure$(if ($flipX -eq 0){$i}else{$bands-$i+1})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color1]
 
 "@
-                        }else{
-                            $shapeContent+=@"
-
-Shape$i=Rectangle $($a*($i-1)), $((1-$flipY)*$barHeight), $barWidth, ($(-1+2*$flipY)*Clamp($barHeight*[Measure$(if ($flipX -eq 0){$i}else{$bands-$i+1})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color$i]
-
-"@
-                        }
                     }
                 }
+
+
                 'mirrorX' {
                     $gr1XOff = $RmAPI.Variable("Group1XOffset")
                     $gr1YOff = $RmAPI.Variable("Group1YOffset")
@@ -140,42 +149,132 @@ TransformationMatrix=$(Math -f cos -v $(Math -f rad -v $(-$angle%360)));$(Math -
 
 "@
                     for ($i=1; $i -lt $bands+1; $i++) {
-                        if ($i -eq 1) {
+                        
                         $shapeContent+=@"
 
-Shape=Rectangle $($barStrokeWidth/2), $((1-$flipY)*$barHeight), $barWidth, ($(-1+2*$flipY)*Clamp($barHeight*[Measure$(if ($flipX -eq 0){$i}else{$bands-$i+1})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color1]
+Shape$(if ($i -ne 1){$i}else{''})=Rectangle $($barStrokeWidth/2), $((1-$flipY)*$barHeight), $barWidth, ($(-1+2*$flipY)*Clamp($barHeight*[Measure$(if ($flipX -eq 0){$i}else{$bands-$i+1})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color1]
 
-"@
-                        }else{
-                            $shapeContent+=@"
-
-Shape$i=Rectangle $($a*($i-1)), $((1-$flipY)*$barHeight), $barWidth, ($(-1+2*$flipY)*Clamp($barHeight*[Measure$(if ($flipX -eq 0){$i}else{$bands-$i+1})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color$i]
-
-"@
-                        }
+"@                   
                     }
                 }
+
+
                 'mirrorY' {
 
                 }
+
+
                 'mirrorXY' {
 
                 }
+
+
                 'reflection' {
 
                 }
             }
         }
+
+
+
+
         'round' {
 
         }
+
+
+
+
         'vector' {
+#Getting variables
+            $vectorBands=$RmAPI.Variable("VectorBands")
+
             $layerCount=$RmAPI.Variable("LayerCount")
+
+            $vectorWidth=$RmAPI.Variable("VectorWidth")
+
+            $vectorHeight=$RmAPI.Variable("VectorHeight")
+
+            $angle = $RmAPI.Variable("Angle")
+
+            $angleSin = Math -f sin -v $($angle%360)
+
+            $angleCos = Math -f cos -v $($angle%360)
+
+            $k=$vectorBands
+
+            $layerHash=@{}
+
+            $mainUnit="Unit-Main=Channels [#Channel] | Handlers MainFFT"
+
+            $Handlers=@"
+"@
+#Getting heights for different layers and putting them into an array for later use
+            $heightArray=@('')
+
             for ($j=1; $j -lt $layerCount+1; $j++) {
-                for ($i=1; $j -lt $vectorBands) {
-                    
-                }
+                $RmAPI.Log('Getting heights')
+                $heightArray+=$RmAPI.Variable("Layer$($j)Height")
             }
+            $RmAPI.Log($heightArray)
+
+            $maxHeight=($heightArray | measure -Maximum).Maximum
+
+#Creating required content
+            for ($j=1; $j -lt $layerCount+1; $j++) {
+                $RmAPI.Log("Creating Layer $j")
+                $vectorLine="Layer$j = 0,$maxHeight | "
+
+                for ($i=1; $i -lt $k+1; $i++) {
+                    $vectorLine+="LineTo $(if ($i -ne 1){$i*($VectorWidth/$k)}else{0}), ($($maxHeight-$heightArray[$j]) + $($heightArray[$j])*(1-[MeasureL$j$i])) | "
+                }
+                $RmAPI.Log("Completed layer $j")
+                $mainUnit+=", Layer$($j)BR(MainFFT), Layer$($j)BCT(Layer$($j)BR), Layer$($j)Output(Layer$($j)BCT)"
+                $layerHash.Add("L$j", $vectorLine)
+                $Handlers+=@"
+
+Handler-Layer$($j)BR=Type BandResampler | Bands Log(Count $($k+1), FreqMin 20, FreqMax 16000) | CubicInterpolation true
+Handler-Layer$($j)BCT=Type BandCascadeTransformer | MixFunction Average | MinWeight 0.01 | TargetWeight 2 | ZeroLevelMultiplier 1
+Handler-Layer$($j)Output=Type TimeResampler | Attack [#Attack] | Decay [#Decay] | Transform dB, Map(From -[#MaxSensitivity] : -[#MinSensitivity]), Clamp
+
+
+"@
+                $k=2*$k-1
+            }
+
+            $audioMeasureContent=@"
+
+[AudioAnalyzer]
+Measure=Plugin
+Plugin=AudioAnalyzer
+Type=Parent
+Source=[#AnalyzerPort]
+ProcessingUnits=Main
+$mainUnit | Filter Custom bqHighPass(Q 0.2, Freq 20, ForcedGain 5.58), bqLowPass(Q 1, Freq 16000, ForcedGain 20)
+Handler-MainFFT=Type FFT | BinWidth 8 | OverlapBoost 10 | CascadesCount 2 | WindowFunction [#WindowFunction]
+$Handlers
+
+"@
+            $shapeContent=@"
+
+[Shape]
+Meter=Shape
+DynamicVariables=1
+W=$($angleCos*$vectorWidth + $angleSin*$maxHeight)
+H=$($angleSin*$vectorWidth + $angleCos*$maxHeight)
+TransformationMatrix=$(Math -f cos -v $(Math -f rad -v $(-$angle%360)));$(Math -f sin -v $(Math -f rad -v $(-$angle%360)));$(Math -f sin -v $(Math -f rad -v $(-$angle%360)));$(Math -f cos -v $(Math -f rad -v $(-$angle%360)));$($(if ($angle%360 -gt 90 -and $angel%360 -lt 270){$angleCos*$vectorWidth}else{0})+$(if ($angle%360 -gt 0 -and $angel%360 -lt 180){$angleSin*$vectorHeight}else{0}));$($(if ($angle%360 -gt 180 -and $angel%360 -lt 360){$angleSin*$vectorWidth}else{0})+$(if ($angle%360 -gt 90 -and $angel%360 -lt 270){$angleCos*$vectorHeight}else{0}))
+
+"@
+            for ($i=1; $i -lt $layerCount+1; $i++) {
+                $shapeContent+=@"
+
+Shape$(if($i -ne 1){$i}else{''})=Path Layer$i | StrokeWidth 0 | Fill Color $($RmAPI.VariableStr("Layer$($i)Color"))
+$($layerHash["L$i"])LineTo $vectorWidth,$maxHeight | ClosePath 1
+
+"@
+            }
+            $audioMeasureContent | Out-File -FilePath $($resources + 'Measures\VectorAnalyzer.inc') -Encoding utf8
+            $shapeContent | Out-File -FilePath $($resources + 'Visualizer\Vector.inc') -Encoding utf8
         }
     }
 }
