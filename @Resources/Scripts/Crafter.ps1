bc29c0c51c7e2fe5fc4c51c52821882496afbfa6
deathcrafter <sahoodeathcrafter@users.noreply.github.com>
2021-06-02 03:08:29 +0530

    Made my own color picker

--- @Resources/Scripts/Crafter.ps1
+++ @Resources/Scripts/Crafter.ps1
@@ -8,7 +8,9 @@ function main {
 function Vector {
     Craft -type vector
 }
-
+function Round {
+    Craft -type round
+}
 function Bar {
 
     $barType=$RmAPI.VariableStr('BarType')
@@ -224,7 +226,7 @@ Shape$(if ($i -ne 1){$i}else{''})=Rectangle $($gr1XOff+$barStrokeWidth/2+($i-1)*
                     for ($i=$bands+1; $i -lt 2*$bands+1; $i++) {
                         $shapeContent+=@"
 
-Shape$i=Rectangle $($gr2XOff+$barStrokeWidth/2+($i-1-$bands)*$a), $(($barHeight+$barStrokeWidth)+$gr2YOff+$gr2flipY*$barHeight), $barWidth, ($(1-2*$gr2flipY)*Clamp($barHeight*[Measure$(if ($gr2flipX -eq 0){$i-$bands}else{2*$bands+1-$i})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color$(if ($gr1flipX -eq 0){$i}else{$bands-$i+1})]
+Shape$i=Rectangle $($gr2XOff+$barStrokeWidth/2+($i-1-$bands)*$a), $(($barHeight+$barStrokeWidth)+$gr2YOff+$gr2flipY*$barHeight), $barWidth, ($(1-2*$gr2flipY)*Clamp($barHeight*[Measure$(if ($gr2flipX -eq 0){$i-$bands}else{2*$bands+1-$i})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color$(if ($gr2flipX -eq 0){$i}else{$bands-$i+1})]
 
 "@
                     }
@@ -233,7 +235,77 @@ Shape$i=Rectangle $($gr2XOff+$barStrokeWidth/2+($i-1-$bands)*$a), $(($barHeight+
 
 
                 'mirrorXY' {
+                    $gr1XOff = $RmAPI.Variable("Group1XOffset")
+                    $gr1YOff = $RmAPI.Variable("Group1YOffset")
+                    $gr1flipX= $RmAPI.Variable("Group1XFlip")
+                    $gr1flipY= $RmAPI.Variable("Group1YFlip")
+                    $gr2XOff = $RmAPI.Variable("Group2XOffset")
+                    $gr2YOff = $RmAPI.Variable("Group2YOffset")
+                    $gr2flipX= $RmAPI.Variable("Group2XFlip")
+                    $gr2flipY= $RmAPI.Variable("Group2YFlip")
+                    $gr3XOff = $RmAPI.Variable("Group3XOffset")
+                    $gr3YOff = $RmAPI.Variable("Group3YOffset")
+                    $gr3flipX= $RmAPI.Variable("Group3XFlip")
+                    $gr3flipY= $RmAPI.Variable("Group3YFlip")
+                    $gr4XOff = $RmAPI.Variable("Group4XOffset")
+                    $gr4YOff = $RmAPI.Variable("Group4YOffset")
+                    $gr4flipX= $RmAPI.Variable("Group4XFlip")
+                    $gr4flipY= $RmAPI.Variable("Group4YFlip")
 
+                    $groupWidthRawTop = $(if (($gr1XOff+($barWidth+$barGap)*$bands-$barGap) -le ($gr2XOff+2*($barWidth+$barGap)*$bands-$barGap)){($gr2XOff+2*($barWidth+$barGap)*$bands-$barGap)}else{($gr1XOff+($barWidth+$barGap)*$bands-$barGap)})
+                    $groupHeightRawTop = $(if ($gr1YOff -ge $gr2YOff){$gr1YOff}else{$gr2YOff})
+
+                    $groupWidthRawBottom = $(if (($gr3XOff+($barWidth+$barGap)*$bands-$barGap) -le ($gr4XOff+2*($barWidth+$barGap)*$bands-$barGap)){($gr4XOff+2*($barWidth+$barGap)*$bands-$barGap)}else{($gr3XOff+($barWidth+$barGap)*$bands-$barGap)})
+                    $groupHeightRawBottom = $(if ($gr3YOff -ge $gr4YOff){$gr3YOff}else{$gr4YOff})
+
+                    $groupWidth = $(if ($groupWidthRawTop -ge $groupWidthRawBottom){$groupWidthRawTop}else{$groupWidthRawBottom})
+                    $groupHeight = $groupHeightRawTop+$groupHeightRawBottom
+
+                    $a = $barWidth + $barGap
+                    $b = $a*$bands
+
+                    $width1 = $groupWidth+$barStrokeWidth
+                    $height1 = 2*($barHeight+$levitate+$barStrokeWidth)+$groupHeight
+
+                    $shapeContent+=@"
+
+[Shape]
+Meter=Shape
+DynamicVariables=1
+W=$($angleCos*$width1 + $angleSin*$height1)
+H=$($angleSin*$width1 + $angleCos*$height1)
+TransformationMatrix=$([math]::Cos(-$angle%360*([math]::PI/180)));$(-[math]::Sin(-$angle%360*([math]::PI/180)));$([math]::Sin(-$angle%360*([math]::PI/180)));$([math]::Cos(-$angle%360*([math]::PI/180)));$($(if ($angle%360 -gt 90 -and $angel%360 -lt 270){$angleCos*$width1}else{0})+$(if ($angle%360 -gt 0 -and $angel%360 -lt 180){$angleSin*$height1}else{0}));$($(if ($angle%360 -gt 180 -and $angel%360 -lt 360){$angleSin*$width1}else{0})+$(if ($angle%360 -gt 90 -and $angel%360 -lt 270){$angleCos*$height1}else{0}))
+
+"@
+                    for ($i=1; $i -lt $bands+1; $i++) {                        
+                        $shapeContent+=@"
+
+Shape$(if ($i -ne 1){$i}else{''})=Rectangle $($gr1XOff+$barStrokeWidth/2+($i-1)*$a), $($gr1YOff+(1-$gr1flipY)*$barHeight), $barWidth, ($(-1+2*$flipY)*Clamp($barHeight*[Measure$(if ($gr1flipX -eq 0){$i}else{$bands-$i+1})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color$(if ($gr1flipX -eq 0){$i}else{$bands-$i+1})]
+
+"@
+                    }
+                    for ($i=$bands+1; $i -lt 2*$bands+1; $i++) {
+                        $shapeContent+=@"
+
+Shape$i=Rectangle $($gr2XOff+$barStrokeWidth/2+$b+($i-1-$bands)*$a), $($gr2YOff+(1-$gr2flipY)*$barHeight), $barWidth, ($(-1+2*$gr2flipY)*Clamp($barHeight*($(if ($gr2Complement -eq 1){'1-'}else{''})[Measure$(if ($gr2flipX -eq 0){2*$bands+1 - $i}else{$i-$bands})]), $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color$(if ($gr2flipX -eq 0){2*$bands+1 - $i}else{$i-$bands})]
+
+"@
+                    }
+                    for ($i=2*$bands+1; $i -lt 3*$bands+1; $i++) {
+                        $shapeContent+=@"
+
+Shape$i=Rectangle $($gr3XOff+$barStrokeWidth/2+($i-1-2*$bands)*$a), $(($barHeight+$barStrokeWidth)+$gr3YOff+$gr3flipY*$barHeight), $barWidth, ($(1-2*$gr3flipY)*Clamp($barHeight*[Measure$(if ($gr3flipX -eq 0){$i-2*$bands}else{3*$bands+1-$i})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color$(if ($gr3flipX -eq 0){$i-2*$bands}else{4*$bands-$i+1})]
+
+"@
+                    }
+                    for ($i=3*$bands+1; $i -lt 4*$bands+1; $i++) {
+                        $shapeContent+=@"
+
+Shape$i=Rectangle $($gr4XOff+$barStrokeWidth/2+$b+($i-1-3*$bands)*$a), $(($barHeight+$barStrokeWidth)+$gr4YOff+$gr4flipY*$barHeight), $barWidth, ($(1-2*$gr4flipY)*Clamp($barHeight*[Measure$(if ($gr4flipX -eq 0){4*$bands+1-$i}else{$i-3*$bands})], $minimumHeight, $barHeight)), $cornerRadius | Fill Color [#Color$(if ($gr4flipX -eq 0){4*$bands+1-$i}else{$i-3*$bands})]
+
+"@
+                    }
+                    break                    
                 }
 
 
@@ -248,7 +320,37 @@ Shape$i=Rectangle $($gr2XOff+$barStrokeWidth/2+($i-1-$bands)*$a), $(($barHeight+
 
 
         'round' {
+            $bands = $RmAPI.Variable("Bands")
 
+            $barWidth= $RmAPI.Variable("BarWidth")
+
+            $height= $RmAPI.Variable("Height")
+
+            $minimumHeight=$RmAPI.Variable("MinimumHeight")
+
+            $radius= $RmAPI.Variable("Radius")
+
+            $angleStart= $RmAPI.Variable("StartAngle")
+
+            $totalAngle= $RmAPI.Variable("TotalAngle")
+
+            $shapeContent=@"
+
+[Shape]
+Meter=Shape
+DynamicVariables=1
+W=$(2*($height+$radius)+$barWidth)
+H=$(2*($height+$radius)+$barWidth)
+
+"@
+
+            for ($i=1; $i -lt $bands+1; $i++) {
+                $shapeContent+=@"
+
+Shape$(if($i -ne 1){$i}else{''})=Line $((-([math]::Cos($(Math -f rad -v $($angleStart+$i*($totalAngle/$bands)))))*$radius) + $height+$radius), $((-([math]::Sin($(Math -f rad -v $($angleStart+$i*($totalAngle/$bands)))))*$radius) + $height+$radius), ($(-([math]::Cos($(Math -f rad -v $($angleStart + $i*$TotalAngle/$bands)))))*($radius+(Clamp($height*[Measure$i], $minimumHeight, $height))) + $($height+$radius)), ($(-([math]::Sin($(Math -f rad -v $($angleStart + $i*$TotalAngle/$bands)))))*($radius+(Clamp($height*[Measure$i], $minimumHeight, $height))) + $($height+$radius)) | StrokeWidth $barWidth | Stroke Color [#Color$i]
+"@
+            }
+            $shapeContent | Out-File -FilePath $($resources + 'Visualizer\Round.inc') -Encoding utf8
         }
 
 
